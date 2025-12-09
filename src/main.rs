d4eb32c81d2a75db54a9718d11423b1326863e86
Kieran Reid <kieranreid2612@gmail.com>
2025-12-09 20:21:44 +0100

    Added eums that implement display for ( & )

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 #![allow(unused_variables)]
-use std::env;
+use std::fmt::Display;
 use std::fs;
+use std::{env, fmt};
+
+enum Token {
+    LeftParen,
+    RightParen,
+    EndOfFile,
+    Unknown,
+}
+
+impl Display for Token {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> fmt::Result {
+        let token = match *self {
+            Self::LeftParen => "LEFT_PAREN ( null",
+            Self::RightParen => "RIGHT_PAREN ) null",
+            Self::EndOfFile => "EOR  null",
+            Self::Unknown => "",
+        };
+
+        write!(fmt, "{}", token)
+    }
+}
 
 // USE
 // impl
@@ -19,22 +40,38 @@ fn main() {
     let filename = &args[2];
 
     match command.as_str() {
-        "tokenize" => {
-            eprintln!("Logs from your program will appear here!");
-
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                eprintln!("Failed to read file {}", filename);
-                String::new()
-            });
-
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
-            }
-        }
+        "tokenize" => tokenize(command, filename),
         _ => {
             eprintln!("Unknown command: {}", command);
         }
     }
+}
+
+fn tokenize(command: &str, filename: &str) {
+    eprintln!("Logs from your program will appear here!");
+
+    let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+        eprintln!("Failed to read file {}", filename);
+        String::new()
+    });
+
+    let mut tokens = file_contents.chars().peekable();
+    let mut output: Vec<Token> = Vec::new();
+
+    if !file_contents.is_empty() {
+        while let Some(token) = tokens.next() {
+            let token = match token {
+                '(' => Token::LeftParen,
+                ')' => Token::RightParen,
+                _ => Token::Unknown,
+            };
+            output.push(token);
+        }
+    }
+
+    for token in output {
+        println!("{}", token)
+    }
+
+    println!("EOF  null");
 }
