5a5e1d8ed75925e4f5c3cb74b49acd3cd3631753
Kieran Reid <kieranreid2612@gmail.com>
2025-12-16 23:48:47 +0000

    Implemented unary operator expressions to evaluator

--- src/executor.rs
+++ src/executor.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use crate::expression::{Expression, Operator, Primary};
+use crate::expression::{Expression, Operator, Primary, Unary};
 use crate::parser::parse;
 
 pub fn execute(filename: &str) {
@@ -10,8 +10,8 @@ pub fn execute(filename: &str) {
 
     let mut output: Vec<String> = Vec::new();
 
-    while let Some(expression) = stream.advance() {
-        let result = evaluate_expression(expression);
+    while !stream.is_at_end() {
+        let result = evaluate_expression(stream.advance().expect("Tokens to exist"), &mut stream);
         output.push(result);
     }
 
@@ -20,20 +20,49 @@ pub fn execute(filename: &str) {
     }
 }
 
-fn evaluate_expression(expression: Expression) -> String {
+fn evaluate_expression(expression: Expression, stream: &mut ExpressionStream) -> String {
     match expression {
-        Expression::Primary(literal) => primary(literal),
+        Expression::Primary(literal) => primary(literal, stream),
+        Expression::Unary(operator, expression) => unary(operator, *expression, stream),
         // Expression::Binary(left, operator, right) => binary(*left, operator, *right),
         _ => panic!("Not implemented"),
     }
 }
 
-fn primary(literal: Primary) -> String {
+fn primary(literal: Primary, stream: &mut ExpressionStream) -> String {
     match literal {
         Primary::Number(_, number, _) => number.to_string(),
         Primary::String(string, _) => string.to_string(),
-        Primary::Grouping(expression) => evaluate_expression(*expression),
+        Primary::Grouping(expression) => evaluate_expression(*expression, stream),
         _ => literal.to_string(),
+    }
+}
+
+fn unary(unary: Unary, expression: Expression, stream: &mut ExpressionStream) -> String {
+    let expression = evaluate_expression(expression, stream);
+
+    match unary {
+        Unary::Minus => {
+            format!("{}{}", unary, expression)
+        }
+        Unary::Bang => check_bang(expression, stream),
+    }
+}
+
+fn check_bang(expression: String, stream: &mut ExpressionStream) -> String {
+    let expression = match stream.peek() {
+        Some(Expression::Unary(Unary::Bang, _)) => {
+            stream.advance();
+            return check_bang(expression, stream);
+        }
+        _ => expression,
+    };
+
+    match expression.as_str() {
+        "true" => "false".to_string(),
+        "nil" => "true".to_string(),
+        "false" => "true".to_string(),
+        _ => "false".to_string(),
     }
 }
 
